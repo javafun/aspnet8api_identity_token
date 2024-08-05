292b9b148e7117ad1ab043423992307d33d7cca4
Vincent Yang <vincent.yang@optimizely.com>
2024-08-05 13:51:27 +1000

    feat: initial commit

---
+++ aspnet8identityapi/MyUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aspnet8identityapi
+{
+    public class MyUser : IdentityUser
+    {
+
+    }
+
+    public class AppDbContext : IdentityDbContext<MyUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
